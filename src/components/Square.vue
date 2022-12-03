ca0173e284dd6d75fce6245f2ce256a21a06fd87
Vsevolod <seven@acceptsib.ru>
2022-12-03 21:03:19 +0700

    сделана таблица с характеристиками

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -38,30 +38,72 @@
         </v-btn>
       </v-col>
     </v-row>
-    <!-- <v-row>
-      <select>
-        <option v-for="num in days" :key="num">
-          {{ num }}
-        </option>
-      </select>
-    </v-row> -->
-  <!-- </div> -->
+    <v-row>
+      <div class="square_PF">
+        <table>
+          <tr>
+            <td>
+              <span>сила воли, характер</span>
+              {{ numbersChar.one }}
+            </td>
+            <td>
+              <span>энергия, харизма</span>
+              {{ numbersChar.two }}
+            </td>
+            <td>
+              <span>познание, творчество</span>
+              {{ numbersChar.three }}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {{ numbersChar.four }}
+            </td>
+            <td>
+              {{ numbersChar.five }}
+            </td>
+            <td>
+              {{ numbersChar.six }}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {{ numbersChar.seven }}
+            </td>
+            <td>
+              {{ numbersChar.eight }}
+            </td>
+            <td>
+              {{ numbersChar.nine }}
+            </td>
+          </tr>
+        </table>
+      </div>
+    </v-row>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import { Dates } from "@/types/main"
 @Options({
-  name: "Square", 
-  props: {
-    msg: String
-  }
+  name: "Square",
 })
 export default class Square extends Vue {
   date = {
     day: "",
     month: "",
     year: ""
+  }
+  numbersChar = {
+    one: "",
+    two: "",
+    three: "",
+    four: "",
+    five: "",
+    six: "",
+    seven: "",
+    eight: "",
+    nine: ""
   }
 
   get items(): Dates {
@@ -72,8 +114,40 @@ export default class Square extends Vue {
     }
   }
 
-  calculateDate() {
+  calculateDate(): void {
     console.log(this.date)
+    const indexMonth = this.items.months.findIndex(month => month === this.date.month)
+    const date = this.date.day + String((indexMonth + 1)) + this.date.year
+    const oneStringPF = date.split("").map(i => Number(i))
+    const oneNumber = oneStringPF.reduce((a, b) => a + b, 0)
+    const twoNumber = String(oneNumber).split("").reduce((a, b) => Number(a) + Number(b), 0)
+    const threeNumber = oneNumber - Number(String(oneStringPF)[0]) * 2
+    const fourNumber = String(threeNumber).split("").reduce((a, b) => Number(a) + Number(b), 0)
+    const twoStringPF = `${oneNumber}${twoNumber}${threeNumber}${fourNumber}`.split("").map(i => Number(i))
+    const numbersChar = [...oneStringPF, ...twoStringPF]
+    numbersChar.forEach(num => {
+      switch(num) {
+        case 1:
+          return this.numbersChar.one += num
+        case 2:
+          return this.numbersChar.two += num
+        case 3:
+          return this.numbersChar.three += num
+        case 4:
+          return this.numbersChar.four += num
+        case 5:
+          return this.numbersChar.five += num
+        case 6:
+          return this.numbersChar.six += num
+        case 7:
+          return this.numbersChar.seven += num
+        case 8:
+          return this.numbersChar.eight += num
+        case 9:
+          return this.numbersChar.nine += num
+      }
+    })
+    this.numbersChar.one
   }
 }
 </script>
@@ -89,6 +163,17 @@ export default class Square extends Vue {
       margin-left: 8px !important;
       max-width: max-content;
     }
+  }
+}
+.square_PF {
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    width: 160px;
+    height: 100px;
+    border: 2px solid gray;
+    text-align: center;
   }
 }
 </style>
